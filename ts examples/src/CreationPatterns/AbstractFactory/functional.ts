aa5d87ce31d360c8bf1250f0b50de6b7f7511d3c
EstebanBedoya <ebedoya@ebedoya.localdomain>
2023-07-26 21:47:29 -0500

    prototype

--- ts examples/src/CreationPatterns/AbstractFactory/functional.ts
+++ ts examples/src/CreationPatterns/AbstractFactory/functional.ts
@@ -1,58 +1,60 @@
-interface AbstractFactory {
-  createProductA(): AbstractProductA;
-  createProductB(): AbstractProductB;
+namespace AbstractFactoryFn {
+  interface AbstractFactory {
+    createProductA(): AbstractProductA;
+    createProductB(): AbstractProductB;
+  }
+
+  const createConcreteFactory1: () => AbstractFactory = () => ({
+    createProductA: () => createConcreteProductA1(),
+    createProductB: () => createConcreteProductB1(),
+  });
+
+  const createConcreteFactory2: () => AbstractFactory = () => ({
+    createProductA: () => createConcreteProductA2(),
+    createProductB: () => createConcreteProductB2(),
+  });
+
+  interface AbstractProductA {
+    usefulFunctionA(): string;
+  }
+
+  const createConcreteProductA1: () => AbstractProductA = () => ({
+    usefulFunctionA: () => "The result of the product A1.",
+  });
+
+  const createConcreteProductA2: () => AbstractProductA = () => ({
+    usefulFunctionA: () => "The result of the product A2.",
+  });
+
+  interface AbstractProductB {
+    usefulFunctionB(): string;
+    anotherUsefulFunctionB(collaborator: AbstractProductA): string;
+  }
+
+  const createConcreteProductB1: () => AbstractProductB = () => ({
+    usefulFunctionB: () => "The result of the product B1.",
+    anotherUsefulFunctionB: (collaborator: AbstractProductA) => {
+      const result = collaborator.usefulFunctionA();
+      return `The result of the B1 collaborating with the (${result})`;
+    },
+  });
+
+  const createConcreteProductB2: () => AbstractProductB = () => ({
+    usefulFunctionB: () => "The result of the product B2.",
+    anotherUsefulFunctionB: (collaborator: AbstractProductA) => {
+      const result = collaborator.usefulFunctionA();
+      return `The result of the B2 collaborating with the (${result})`;
+    },
+  });
+
+  function clientCode(factory: AbstractFactory) {
+    const productA = factory.createProductA();
+    const productB = factory.createProductB();
+
+    console.log(productA.usefulFunctionA());
+    console.log(productB.usefulFunctionB());
+    console.log(productB.anotherUsefulFunctionB(productA));
+  }
+
+  clientCode(createConcreteFactory1());
 }
-
-const createConcreteFactory1: () => AbstractFactory = () => ({
-  createProductA: () => createConcreteProductA1(),
-  createProductB: () => createConcreteProductB1(),
-});
-
-const createConcreteFactory2: () => AbstractFactory = () => ({
-  createProductA: () => createConcreteProductA2(),
-  createProductB: () => createConcreteProductB2(),
-});
-
-interface AbstractProductA {
-  usefulFunctionA(): string;
-}
-
-const createConcreteProductA1: () => AbstractProductA = () => ({
-  usefulFunctionA: () => "The result of the product A1.",
-});
-
-const createConcreteProductA2: () => AbstractProductA = () => ({
-  usefulFunctionA: () => "The result of the product A2.",
-});
-
-interface AbstractProductB {
-  usefulFunctionB(): string;
-  anotherUsefulFunctionB(collaborator: AbstractProductA): string;
-}
-
-const createConcreteProductB1: () => AbstractProductB = () => ({
-  usefulFunctionB: () => "The result of the product B1.",
-  anotherUsefulFunctionB: (collaborator: AbstractProductA) => {
-    const result = collaborator.usefulFunctionA();
-    return `The result of the B1 collaborating with the (${result})`;
-  },
-});
-
-const createConcreteProductB2: () => AbstractProductB = () => ({
-  usefulFunctionB: () => "The result of the product B2.",
-  anotherUsefulFunctionB: (collaborator: AbstractProductA) => {
-    const result = collaborator.usefulFunctionA();
-    return `The result of the B2 collaborating with the (${result})`;
-  },
-});
-
-function clientCodeFn(factory: AbstractFactory) {
-  const productA = factory.createProductA();
-  const productB = factory.createProductB();
-
-  console.log(productA.usefulFunctionA());
-  console.log(productB.usefulFunctionB());
-  console.log(productB.anotherUsefulFunctionB(productA));
-}
-
-clientCodeFn(createConcreteFactory1());
--- ts examples/src/CreationPatterns/FactoryMethod/index.ts
+++ ts examples/src/CreationPatterns/FactoryMethod/index.ts
@@ -1,47 +1,51 @@
-interface Product {
-  operation(): string;
-}
-
-abstract class Creator {
-  public abstract createProduct(): Product;
-
-  public someOperation(): string {
-    const product = this.createProduct();
-    return `Creator: The same creator's code has just worked with ${product.operation()}`;
+namespace FactoryMethodClass {
+  interface Product {
+    operation(): string;
   }
-}
 
-class ConcreteCreatorA extends Creator {
-  public createProduct(): Product {
-    return new ConcreteProductA();
+  abstract class Creator {
+    public abstract createProduct(): Product;
+
+    public someOperation(): string {
+      const product = this.createProduct();
+      return `Creator: The same creator's code has just worked with ${product.operation()}`;
+    }
   }
-}
 
-class ConcreteCreatorB extends Creator {
-  public createProduct(): Product {
-    return new ConcreteProductB();
+  class ConcreteCreatorA extends Creator {
+    public createProduct(): Product {
+      return new ConcreteProductA();
+    }
   }
-}
 
-class ConcreteProductA implements Product {
-  public operation(): string {
-    return '{Result of the ConcreteProductA}';
+  class ConcreteCreatorB extends Creator {
+    public createProduct(): Product {
+      return new ConcreteProductB();
+    }
   }
-}
 
-class ConcreteProductB implements Product {
-  public operation(): string {
-    return '{Result of the ConcreteProductB}';
+  class ConcreteProductA implements Product {
+    public operation(): string {
+      return "{Result of the ConcreteProductA}";
+    }
   }
+
+  class ConcreteProductB implements Product {
+    public operation(): string {
+      return "{Result of the ConcreteProductB}";
+    }
+  }
+
+  function clientCode(creator: Creator) {
+    console.log(
+      "Client: I'm not aware of the creator's class, but it still works."
+    );
+    console.log(creator.someOperation());
+  }
+
+  console.log("App: Launched with the ConcreteCreatorA.");
+  clientCode(new ConcreteCreatorA());
+
+  console.log("App: Launched with the ConcreteCreatorB.");
+  clientCode(new ConcreteCreatorB());
 }
-
-function clientCode(creator: Creator) {
-  console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
-  console.log(creator.someOperation());
-}
-
-console.log('App: Launched with the ConcreteCreatorA.');
-clientCode(new ConcreteCreatorA());
-
-console.log('App: Launched with the ConcreteCreatorB.');
-clientCode(new ConcreteCreatorB());
